46d613bc81b27172701542561fff201c7c3489bd
Elmer Pineda <mhelpineda14@gmail.com>
2026-03-09 17:10:55 +0800

    update activity 11

--- 11-EthersInAndOut.sol
+++ 11-EthersInAndOut.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.31;
 contract PayableFuncContract {
     // ACTIVITY 1 — First Payable Function
     mapping(address => uint) balances;
+    event Withdrawn(address user, uint amount);
 
     function deposit() external payable {
         require(msg.value > 0, "Amount must be greater than 0");
@@ -14,12 +15,12 @@ contract PayableFuncContract {
         return balances[_wallet];
     }
 
-    // ======================================
     // ACTIVITY 2 — Withdraw Your Funds
     function withdraw(uint _amount) external {
         require(balances[msg.sender] >= _amount, "Insufficient balance");
         balances[msg.sender] -= _amount;
         (bool success, ) = payable(msg.sender).call{value: _amount}("");
         require(success, "Withdraw failed");
+        emit Withdrawn(msg.sender, _amount);
     }
 }
